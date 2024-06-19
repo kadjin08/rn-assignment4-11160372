7a8c74069156795730ea29d023689b97caced284
kadjin08 <knaayitey-adjin@st.ug.edu.gh>
2024-06-19 22:36:49 +0000

    restarted project

---
+++ appC08/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import LoginPage from './LoginPage';
+import HomePage from './HomePage';
+
+
+const Stack = createStackNavigator();
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginPage} options={{ headerShown: false }} />
+        <Stack.Screen name="Home" component={HomePage} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
